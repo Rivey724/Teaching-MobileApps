1f1f14566256c89bbd5b4c8ea4e01205e6af6033
Rivey724 <Rivey724@gmail.com>
2018-01-24 14:45:32 -0800

    Update

--- projects/Project1/Project1/MainActivity.cs
+++ projects/Project1/Project1/MainActivity.cs
@@ -21,7 +21,75 @@ namespace Project1
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
             button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            Button button1 = FindViewById<Button>(Resource.Id.button1);
+            Button button2 = FindViewById<Button>(Resource.Id.button2);
+            Button button3 = FindViewById<Button>(Resource.Id.button3);
+            Button button4 = FindViewById<Button>(Resource.Id.button4);
+            Button button5 = FindViewById<Button>(Resource.Id.button5);
+            Button button6 = FindViewById<Button>(Resource.Id.button6);
+            Button button7 = FindViewById<Button>(Resource.Id.button7);
+            Button button8 = FindViewById<Button>(Resource.Id.button8);
+            Button button9 = FindViewById<Button>(Resource.Id.button9);
+            Button button0 = FindViewById<Button>(Resource.Id.button0);
+            Button buttonPlus = FindViewById<Button>(Resource.Id.buttonPlus);
+            Button buttonMin = FindViewById<Button>(Resource.Id.buttonMin);
+            Button buttonDiv = FindViewById<Button>(Resource.Id.buttonDiv);
+            Button buttonMult = FindViewById<Button>(Resource.Id.buttonMult);
+            Button buttonClear = FindViewById<Button>(Resource.Id.Clear);
+            Button buttonDelete = FindViewById<Button>(Resource.Id.Delete);
+            Button buttonSign = FindViewById<Button>(Resource.Id.SignChange);
+            Button buttonDot = FindViewById<Button>(Resource.Id.Dot);
+            Button buttonEqual = FindViewById<Button>(Resource.Id.Equal);
+
+            button1.Click += Button_Click;
+            button2.Click += Button_Click;
+            button3.Click += Button_Click;
+            button4.Click += Button_Click;
+            button5.Click += Button_Click;
+            button6.Click += Button_Click;
+            button7.Click += Button_Click;
+            button8.Click += Button_Click;
+            button9.Click += Button_Click;
+            button0.Click += Button_Click;
+            buttonPlus.Click += Button_Click;
+            buttonMin.Click += Button_Click;
+            buttonDiv.Click += Button_Click;
+            buttonMult.Click += Button_Click;
+            buttonClear.Click += ButtonClear_Click;
+            buttonDelete.Click += Button_Click;
+            buttonSign.Click += Button_Click;
+            buttonDot.Click += Button_Click;
+            buttonEqual.Click += ButtonEqual_Click;
+
+
         }
+
+        private void ButtonEqual_Click(object sender, System.EventArgs e)
+        {
+            double Input1 = 0.0;
+            double Input2 = 0.0;
+            double Result = 0.0;
+        }
+
+        private void ButtonClear_Click(object sender, System.EventArgs e)
+        {
+            TextView output = FindViewById<TextView>(Resource.Id.textView1);
+            output.Text = "";
+        }
+
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Button ViewButton = sender as Button;
+            if(ViewButton != null)
+            {
+                TextView output = FindViewById<TextView>(Resource.Id.textView1);
+                output.Text += ViewButton.Text;
+            }
+        }
+
+        
     }
 }
 
--- projects/Project1/Project1/Resources/Resource.Designer.cs
+++ projects/Project1/Project1/Resources/Resource.Designer.cs
@@ -44,38 +44,41 @@ namespace Project1
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int button1 = 2131034115;
-			
-			// aapt resource value: 0x7f05000c
-			public const int button10 = 2131034124;
-			
 			// aapt resource value: 0x7f05000d
-			public const int button11 = 2131034125;
+			public const int Clear = 2131034125;
 			
-			// aapt resource value: 0x7f05000e
-			public const int button12 = 2131034126;
+			// aapt resource value: 0x7f050011
+			public const int Delete = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int Dot = 2131034130;
+			
+			// aapt resource value: 0x7f050013
+			public const int Equal = 2131034131;
 			
 			// aapt resource value: 0x7f05000f
-			public const int button13 = 2131034127;
+			public const int SignChange = 2131034127;
 			
-			// aapt resource value: 0x7f050010
-			public const int button14 = 2131034128;
+			// aapt resource value: 0x7f05000e
+			public const int button0 = 2131034126;
 			
-			// aapt resource value: 0x7f050004
-			public const int button2 = 2131034116;
+			// aapt resource value: 0x7f050001
+			public const int button1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int button2 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int button3 = 2131034115;
 			
 			// aapt resource value: 0x7f050005
-			public const int button3 = 2131034117;
+			public const int button4 = 2131034117;
 			
 			// aapt resource value: 0x7f050006
-			public const int button4 = 2131034118;
+			public const int button5 = 2131034118;
 			
 			// aapt resource value: 0x7f050007
-			public const int button5 = 2131034119;
-			
-			// aapt resource value: 0x7f050008
-			public const int button6 = 2131034120;
+			public const int button6 = 2131034119;
 			
 			// aapt resource value: 0x7f050009
 			public const int button7 = 2131034121;
@@ -86,11 +89,17 @@ namespace Project1
 			// aapt resource value: 0x7f05000b
 			public const int button9 = 2131034123;
 			
-			// aapt resource value: 0x7f050001
-			public const int myButton = 2131034113;
+			// aapt resource value: 0x7f050010
+			public const int buttonDiv = 2131034128;
 			
-			// aapt resource value: 0x7f050002
-			public const int myButton2 = 2131034114;
+			// aapt resource value: 0x7f050008
+			public const int buttonMin = 2131034120;
+			
+			// aapt resource value: 0x7f05000c
+			public const int buttonMult = 2131034124;
+			
+			// aapt resource value: 0x7f050004
+			public const int buttonPlus = 2131034116;
 			
 			// aapt resource value: 0x7f050000
 			public const int textView1 = 2131034112;
